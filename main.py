938c16e0e5cbdd503122fd7570ef8fc1e8e8e742
Oye Goink <98343793+DudeWhatsupDude@users.noreply.github.com>
2022-12-05 16:50:39 +0000

    Initial setting up of particle system

--- main.py
+++ main.py
@@ -6,7 +6,15 @@ DISPLAYSURF = pygame.display.set_mode((400, 300))
 pygame.display.set_caption('Hello World!')
 
 class Particle:
-  def __init__(self, s_vel, gravity, )
+  def __init__(self, s_vel, gravity, window_size, pos):
+    self.x = pos[0]
+    self.y = pos[1]
+    self.xvel = s_vel[0]
+    self.yvel = s_vel[1]
+    self.Gravity = gravity
+    self.window_x = window_size[0]
+    self.window_y = window_size[1]
+
 
 while True:
     for event in pygame.event.get():
