1d96b2f0e80dc9090b04dcadebbb03bd27c351a9
Oye Goink <98343793+DudeWhatsupDude@users.noreply.github.com>
2022-12-05 19:17:16 +0000

    Stuff

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ DISPLAYSURF = pygame.display.set_mode((400, 300))
 pygame.display.set_caption('Hello World!')
 
 class Particle:
-  def __init__(self, s_vel, gravity, window_size, pos):
+  def __init__(self, s_vel, gravity, window_size, pos, screen, friction):
     self.x = pos[0]
     self.y = pos[1]
     self.xvel = s_vel[0]
@@ -14,8 +14,18 @@ class Particle:
     self.Gravity = gravity
     self.window_x = window_size[0]
     self.window_y = window_size[1]
-
-
+    self.screen = screen
+    self.Xfriction = friction[0]
+    self.Yfriction = friction[1]
+    
+  def Tick(self):
+    self.yvel += self.gravity
+    self.xvel *= self.Xfriction
+    self.yvel *= self.Yfriction
+    self.x += self.xvel
+    self.y += self.yvel
+  def Show(self):
+    pygame.draw.circle(self.screen, (255,0,0), (self.x,self.y), 20)
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
